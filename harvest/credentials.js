45c2539f785f9cf2302a27339e1984b5d0e2f2eb
Alessio Biancalana <dottorblaster@gmail.com>
2013-01-24 23:27:44 +0100

    sample files

---
+++ harvest/credentials.js
@@ -0,0 +1,8 @@
+ var credentials = {
+    consumer_key: 'your consumer key here',
+    consumer_secret: 'your consumer secret here',
+    access_token_key: 'your access token key here',
+    access_token_secret: 'your access token secret here'
+};
+
+module.exports = credentials;
---
+++ harvest/harvest.js
@@ -0,0 +1,19 @@
+var twitter = require('ntwitter');
+var credentials = require('./credentials.js');
+
+var t = new twitter({
+    consumer_key: credentials.consumer_key,
+    consumer_secret: credentials.consumer_secret,
+    access_token_key: credentials.access_token_key,
+    access_token_secret: credentials.access_token_secret
+});
+
+t.stream(
+    'statuses/filter',
+    { track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
+    function(stream) {
+        stream.on('data', function(tweet) {
+            console.log(tweet.text);
+        });
+    }
+);
