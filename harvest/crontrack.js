66e5dbfb380aea2f96e7608b52acc8fca7858135
Alessio Biancalana <dottorblaster@gmail.com>
2013-01-25 17:22:36 +0100

    updating crontrack.js with mongo structure

--- harvest/crontrack.js
+++ harvest/crontrack.js
@@ -1,5 +1,16 @@
 var twitter = require('ntwitter');
+var mongoose = require('mongoose');
 var credentials = require('./credentials.js');
+
+var twschema = new mongoose.Schema({
+    tw_id : String,
+    tw_text : String,
+    tw_userid : String,
+    tw_name : String,
+    in_reply_to_status_id : String,
+    in_reply_to_screen_name : String,
+    tw_truncated : Boolean
+  });
 
 var t = new twitter({
     consumer_key: credentials.consumer_key,
