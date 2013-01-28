4994d00138dd138a149f72798bb285c49c20395c
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2013-01-29 00:46:20 +0100

    rework avatar async importer

--- import/import-avatar.js
+++ import/import-avatar.js
@@ -3,6 +3,7 @@ var request = require('request');
 var mongoose = require('mongoose');
 var fs = require('fs')
 var async = require('async');
+var http = require('http');
 
 // hack per fs
 fs.exists = fs.exists || require('path').exists;
@@ -18,64 +19,42 @@ if (!fs.existsSync("./public/"+DIRNAME)) {
     fs.mkdirSync("./public/"+DIRNAME);
 }
 
-
+process.setMaxListeners(0);
 db.once('open', function() {
-    function saveurl(url, usr){
+    function saveurl(url, usr, callback){
         // nome generico
-        var localurl = DIRNAME + "/"+usr._id+ ".photo";//.jpeg";
-        request(url, function(error, response, body){
+        var localurl = DIRNAME + "/" + usr._id + ".photo";//.jpeg";
+        var r = request(url, function(error, response, body){
             if(!error && response.statusCode == 200 ){
                 usr.mep_epFotoUrl = config.base_path + localurl;
                 usr.save();
             };
-            //id = fs.writeFile("./public/" + localurl, response.body);
-            //fs.close(id);
-        }).pipe(fs.createWriteStream("./public/" + localurl));
+            //id = fs.writeFile("./public/" + localurl, body);
+        }).pipe(fs.createWriteStream("./public/" + localurl));        
+
+        // fire on close
+        r.on('close', function () { 
+          callback();
+        });
     }
 
     var mepSchema = config.schema
     var MepModel = db.model(config.db_collection, mepSchema);
-
-    // limits 
-    async.series([
-        function() { 
-          MepModel.find( function(err,mep){
-              mep.forEach( function(i){
-                  var url = "http://api.twitter.com/1/users/profile_image?screen_name="+ i.mep_twitterUrl + "&size=bigger";
-                  console.log("now handle: "+url);
-                  saveurl(url,i);
-              });
-          });
-        },
-        function() {
-           console.log('Avatars imported');
-           mongoose.disconnect();
-           process.exit(0);
-        }
-    ], function(e) {});
     
-   /*
-    MepModel.find(function(err, mep) {
-        async.forEach(mep, function(i) {
-            var url = "http://api.twitter.com/1/users/profile_image?screen_name="+ i.mep_twitterUrl + "&size=bigger";
-            console.log("now handle: " + url);
-            saveurl(url, i);
+    // avatar importer async version
+    var counter = 0;
+    MepModel.find({}, function(err, meps) { 
+      count = meps.length;
+      meps.forEach(function(i) {
+        var url = "http://api.twitter.com/1/users/profile_image?screen_name="+ i.mep_twitterUrl + "&size=bigger";
+        saveurl(url, i, function() {
+          counter++; 
+          if (counter == meps.length) {
+            console.log('Imported: ' + counter + ' avatars.');
+            mongoose.disconnect(); 
+            process.exit();
+          }
         });
+      });
     });
-    MepModel.find( function(err,mep){
-        mep.forEach( function(i){
-            var url = "http://api.twitter.com/1/users/profile_image?screen_name="+ i.mep_twitterUrl + "&size=bigger";
-            console.log("now handle: "+url);
-            saveurl(url,i);
-        });
-    });
-
-    */
-
-
-    // very end, need to give enough time to finish all async stuff.
-    
-    setTimeout( function () {
-     mongoose.disconnect();
-    }, 30000);
 });
