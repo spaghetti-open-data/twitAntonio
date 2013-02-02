c34668f206963d76112a468fb8632c467c72bc51
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2013-02-02 17:24:43 +0100

    fix harvester twitter configuration

--- harvest/harvest-cron.js
+++ harvest/harvest-cron.js
@@ -7,10 +7,10 @@ var config = require('../config.js');
 var twschema = new mongoose.Schema(config.tw_harvest_schema, {autoindex: true});
 
 var t = new twitter({
-    consumer_key: credentials.consumer_key,
-    consumer_secret: credentials.consumer_secret,
-    access_token_key: credentials.access_token_key,
-    access_token_secret: credentials.access_token_secret
+    consumer_key: credentials.twit_consumer_key,
+    consumer_secret: credentials.twit_consumer_secret,
+    access_token_key: credentials.twit_access_token,
+    access_token_secret: credentials.twit_token_secret
 });
 
 // connect to dataabse
