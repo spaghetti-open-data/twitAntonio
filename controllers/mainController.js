489db03883dcb3c091abee481f744e7092dc0c00
Paolo Mainardi (twinbit) <paolo@twinbit.it>
2013-02-02 17:24:52 +0100

    spaces

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -35,6 +35,7 @@ module.exports = function() {
        // sorting methods:
        sorting = (req.query.sorting ? req.query.sorting : '');
        sorting_order = (req.query.sorting_order ? req.query.sorting_order : '');
+
        // update options object to match the sort type:
        if ( req.query.sort_type == "on" ) options['sort_type'] = 'desc';
        if( sorting == 'name' ) options['sort_attrib'] = 'mep_lastName' ;
@@ -43,7 +44,7 @@ module.exports = function() {
        else if( sorting == 'tweets' ) options['sort_attrib'] = 'mep_tweet_count' ;
        else if( sorting == 'followers' ) options['sort_attrib'] = 'mep_follower_count' ;
        else if( sorting == 'lastTweet' ) options['sort_attrib'] = 'mep_last_tweet' ;
-       else{
+       else {
          var rndMethods = ['mep_lastName','mep_firstName','mep_localParty','mep_country'];
          var idx = Math.floor( Math.random() * ( rndMethods.length ) );
          var ascdesc = !! Math.round(Math.random() * 1);
