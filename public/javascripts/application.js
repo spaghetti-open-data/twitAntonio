97655b2f7ba2510bbbbc907c4e4af1b110c4efe2
Andrea Gasparini <gaspa@yattaweb.it>
2013-02-03 23:52:32 +0100

    temp

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -60,5 +60,47 @@
           //twttr.events.bind('click', twantonio_clickEventToAnalytics);
           //twttr.events.bind('tweet', twantonio_clickEventToAnalytics);
         });
+
+        // 
+
     })
 }(window.jQuery)
+
+var count = 1;
+$(document).ready( function(){
+        // each page has 20 entries
+        function getBaseUrl(){
+            var baseurl = location.href.slice(location.origin.length);
+            var patt = /&limit=(\d+)/;
+            var limit = patt.exec( baseurl )
+            if(! limit ) return baseurl+"&limit=" + (20*count);
+            baseurl.replase( patt, "&limit="+(limit+20*count));
+        }
+        $('#content').scrollPagination({
+            'contentPage':  getBaseUrl,
+            'contentData': {}, // these are the variables you can pass to the request, for example: children().size() to know which page you are
+            'scrollTarget': $(window), // who gonna scroll? in this example, the full window
+            'heightOffset': 10, // it gonna request when scroll is 10 pixels before the page ends
+            'beforeLoad': function(){ // before load function, you can display a preloader div
+                $('#loading').fadeIn();
+            },
+            'afterLoad': function(elementsLoaded){ // after loading content, you can use this function to animate your new elements
+                 $('#loading').fadeOut();
+                 count += 1;
+                 $(elementsLoaded).fadeInWithDelay();
+                 if ($('#content').children().size() > 100){ // if more than 100 results already loaded, then stop pagination (only for testing)
+                    $('#nomoreresults').fadeIn();
+                    $('#content').stopScrollPagination();
+                 }
+            }
+        });
+
+        // code for fade in element by element
+        $.fn.fadeInWithDelay = function(){
+            var delay = 0;
+            return this.each(function(){
+                $(this).delay(delay).animate({opacity:1}, 200);
+                delay += 100;
+            });
+        };
+});
